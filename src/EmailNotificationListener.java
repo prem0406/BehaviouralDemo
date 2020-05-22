bed1d6caa624a1be0ba511feb97c4ddbc81d9e97
prem0406 <premk191@gmail.com>
2020-05-22 17:57:55 +0530

    commit

---
+++ src/EmailNotificationListener.java
@@ -0,0 +1,14 @@
+
+
+public class EmailNotificationListener implements EventListener {
+    private String email;
+
+    public EmailNotificationListener(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void update(String eventType, String description) {
+        System.out.println("Email to " + email + ": " +eventType +" : "+ description);
+    }
+}
---
+++ src/EventListener.java
@@ -0,0 +1,5 @@
+
+
+public interface EventListener {
+    void update(String eventType, String description);
+}
